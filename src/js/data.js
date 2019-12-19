84d0ea9f16c03ccc64c894d46882efcd879a6d65
Gal Ben Haim <Gal.BenHaim@solaredge.com>
2019-12-19 23:47:53 +0200

    Fix images path

--- src/js/data.js
+++ src/js/data.js
@@ -1,10 +1,10 @@
 var cards = [
-  { src: 'assets/1.jpeg', id: 0, key: '0' },
-  { src: 'assets/2.jpeg', id: 1, key: '1' },
-  { src: 'assets/1.jpeg', id: 3, key: '0' },
-  { src: 'assets/2.jpeg', id: 4, key: '1' },
-  { src: 'assets/3.jpg', id: 5, key: '3' },
-  { src: 'assets/3.jpg', id: 6, key: '3' },
-  { src: 'assets/4.jpg', id: 7, key: '4' },
-  { src: 'assets/4.jpg', id: 8, key: '4' },
+  { src: 'src/assets/1.jpeg', id: 0, key: '0' },
+  { src: 'src/assets/2.jpeg', id: 1, key: '1' },
+  { src: 'src/assets/1.jpeg', id: 3, key: '0' },
+  { src: 'src/assets/2.jpeg', id: 4, key: '1' },
+  { src: 'src/assets/3.jpg', id: 5, key: '3' },
+  { src: 'src/assets/3.jpg', id: 6, key: '3' },
+  { src: 'src/assets/4.jpg', id: 7, key: '4' },
+  { src: 'src/assets/4.jpg', id: 8, key: '4' },
 ];
